73da88c3f91651ee1e23fb60298b36036d730c6f
patricia243 <21tb542@esisalama.org>
2024-05-28 11:34:37 +0200

    on est dans la classe homeScreem

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,11 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Kotlin serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    // Retrofit with Kotlin serialization Converter
+
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
 }
